830f235924953067b1a638bbd3caeb43552671e6
Bshar <basharnasany.1999@gmail.com>
2026-01-15 16:23:53 +0300

    Add Netlify URL to CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT ||  9000
 app.use(express.json())
 app.use(cors({
     origin: [  "http://localhost:5173",
-            "https://computer-store12.netlify.app/" ],
+            "https://computer-store12.netlify.app" ],
             methods : ['GET' , 'POST' , 'DELETE', 'PUT'],
             credentials: true,
 }))
